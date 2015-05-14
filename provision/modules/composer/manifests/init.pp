254e72721edaa003b8a46aa876a9d3d6fd93974b
ridesoft <maurizio.brioschi@ridesoft.org>
2015-05-14 10:44:32 +0200

    fix bug mv composer globally

--- provision/modules/composer/manifests/init.pp
+++ provision/modules/composer/manifests/init.pp
@@ -4,11 +4,16 @@ ensure => present
 }
 exec { 'installcomposer':
 command => '/usr/bin/curl -sS https://getcomposer.org/installer | php -- --install-dir=/home/vagrant',
-require => Package['curl','php5']
+require => Package['curl','php5'],
+before => Exec['movecomposerglobally']
+}
+file{ '/home/vagrant/composer.phar':
+  ensure=>file,
+  require => Exec['installcomposer']
 }
 exec { 'movecomposerglobally':
 command => '/bin/mv /home/vagrant/composer.phar /usr/bin/composer',
-require => Package['curl','php5']
+require => File['/home/vagrant/composer.phar']
 }
 
 }
