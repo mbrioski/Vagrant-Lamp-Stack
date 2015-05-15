ca82e81e59d717f816a3f2e7e2bceb9848eeb4bf
ridesoft <maurizio.brioschi@ridesoft.org>
2015-05-15 20:38:25 +0200

    some fucked xdebug configuration that doesn't work!

--- provision/modules/php/manifests/init.pp
+++ provision/modules/php/manifests/init.pp
@@ -9,9 +9,10 @@ class php54 {
 
   package { "php5":
   ensure => latest,
-
   }
-
+  package { "php5-fpm":
+  ensure => present,
+  }
   package { "php5-mysql":
   ensure => present,
   require => Package['php5']
@@ -72,10 +73,68 @@ class php54 {
   ensure => present,
   require => [Package['php5'], Package['apache2']]
   }
-  package{ 'xdebug':
+  package{ 'php5-xdebug':
     name => 'php5-xdebug',
     ensure=> installed,
     require=>Package["php5"]
   }
+  file { '/etc/php5/conf.d/xdebug.ini':
+      source => 'puppet:///modules/php/xdebug.ini',
+      require => Package['php5-xdebug'],
+  }
+  file { '/etc/php5/cli/conf.d/xdebug.ini':
+      source => 'puppet:///modules/php/xdebug.ini',
+      require => [Package['php5-xdebug'],Package['php5-cli']]
+  }
+  file { '/etc/php5/fpm/conf.d/xdebug.ini':
+      source => 'puppet:///modules/php/xdebug.ini',
+      require => [Package['php5-xdebug'],Package['php5-fpm']]
+  }
 
+  file { '/etc/php5/apache2/php.ini':
+    ensure=>present
+  }
+
+  file_line { 'zend_extension=/usr/lib/php5/20121212/xdebug.so':
+    path=>'/etc/php5/apache2/php.ini',
+    line=>'zend_extension=/usr/lib/php5/20121212/xdebug.so'
+  }
+  file_line { 'xdebug.remote_connect_back=1':
+    path=>'/etc/php5/apache2/php.ini',
+    line=>'xdebug.remote_connect_back=1',
+  }
+
+  file_line { 'xdebug.remote_enable= 1':
+    path=>'/etc/php5/apache2/php.ini',
+    line=>'xdebug.remote_enable= 1',
+  }
+
+  file_line { 'xdebug.remote_handler= dbgp':
+    path=>'/etc/php5/apache2/php.ini',
+    line=>'xdebug.remote_handler= dbgp',
+  }
+
+  file_line { 'xdebug.remote_port= 9000':
+    path=>'/etc/php5/apache2/php.ini',
+    line=>'xdebug.remote_port= 9000',
+  }
+
+  file_line { 'xdebug.remote_host=127.0.0.1':
+    path=>'/etc/php5/apache2/php.ini',
+    line=>'xdebug.remote_host=127.0.0.1',
+  }
+
+  file_line { 'xdebug.profiler_enable= 1':
+    path=>'/etc/php5/apache2/php.ini',
+    line=>'xdebug.profiler_enable= 1',
+  }
+
+  file_line { 'xdebug.idekey= "vagrant"':
+    path=>'/etc/php5/apache2/php.ini',
+    line=>'xdebug.idekey= "vagrant"',
+  }
+  exec{'/etc/init.d/php5-fpm restart':
+    command=>'/etc/init.d/php5-fpm restart',
+    require=>File['/etc/php5/cli/conf.d/xdebug.ini']
+  }
 }
